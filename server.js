851bd696c4d9b242c50a23c81128612f2fcc9b2a
kaushalsatam <kaushalabd17@gmail.com>
2024-04-07 17:11:35 +0530

    minor change

--- server.js
+++ server.js
@@ -6,29 +6,24 @@ import pg from 'pg';
 const app = express();
 const port = 3000;
 const db = new pg.Client({
-    user: 'postgres',
-    host: 'localhost',
-    db: 'booknotes',
-    password: 'kaushal22',
-    port: 5432
+    user: "postgres",
+    host: "localhost",
+    database: "booknotes",
+    password: "kaushal22",
+    port: 5432,
 });
 db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const data = [];
-
-// async function getBookCovers() {
-// const getBookCoverData = await axios.get("https://covers.openlibrary.org/b/isbn/9781544507859.json?default=false");
-// const bookCoverData = getBookCoverData.data;
-// const bookCoverUrl = bookCoverData.source_url;
-// }
+let data = [];
 
 app.get("/", async (req, res) => {
     try {
         const queryResult = await db.query("SELECT * FROM books;");
         data = queryResult.rows;
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
