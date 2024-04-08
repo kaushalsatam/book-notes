76db16278966003fec8f0704974a6257b54c621f
kaushalsatam <kaushalabd17@gmail.com>
2024-04-08 10:59:46 +0530

    home page viewing

--- server.js
+++ server.js
@@ -18,12 +18,26 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let data = [];
+let bookCovers = [];
 
 app.get("/", async (req, res) => {
     try {
-        const queryResult = await db.query("SELECT * FROM books;");
+        const queryResult = await db.query("SELECT b.id, b.name, b.author, b.isbn, b.date_read, b.rating, n.book_summary FROM books b JOIN notes n ON b.id = n.book_id;");
         data = queryResult.rows;
-        console.log(data);
+        // console.log(data);
+        // Fetch book covers asynchronously for each book
+        const bookCoverPromises = data.map(async (record) => {
+            const isbnNumber = parseInt(record.isbn);
+            const getBookCovers = await axios.get(`https://covers.openlibrary.org/b/isbn/${isbnNumber}.json?default=false`);
+            return getBookCovers.data["source_url"];
+        });
+        // Wait for all book cover requests to complete
+        bookCovers = await Promise.all(bookCoverPromises);
+        console.log(bookCovers);
+        res.render("index.ejs", {
+            data: data,
+            bookCovers: bookCovers
+        });
     } catch (error) {
         console.log(error);
     }
